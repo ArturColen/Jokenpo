cc377586226158c31b73d87f5274363fa1144514
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:32:18 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static Jokenpo startGame() {
-        // Initializing the game
         System.out.println("******** Jokenpô ********\n");
 
-        // Allow the user to enter his name
         Scanner scan = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
         String playerName = scan.next().toUpperCase();
 
-        // Create the players' objects
         Player user = new Player(playerName);
         Player IA = new Player("IA");
 
-        // Allow the user to enter how many rounds he wants to play
         System.out.print(playerName + ", informe quantas rodadas você deseja jogar: ");
         int rounds = scan.nextInt();
 
@@ -22,9 +18,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Call the methods for running the game
-        Jokenpo Jokenpo = startGame(); // Start the game
-        Jokenpo.toPlay(); // Makes the game
-        Jokenpo.showFinalResult(); // Show the result
+        Jokenpo Jokenpo = startGame();
+        Jokenpo.toPlay();
+        Jokenpo.showFinalResult();
     }
 }
