f0e60f077f6882573496870ed2363c218774b7cf
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:32:41 -0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,16 @@
 public class Player {
-    // Player Attributes
     private String name;
     private Integer score;
 
-    // Build the object
     public Player(String name) {
         this.name = name;
         this.score = 0;
     }
 
-    // Add one point for the player who wins the round
     public void incrementScore() {
         setScore(getScore() + 1);
     }
 
-    // Accessory methods
     public String getName() {
         return name;
     }
